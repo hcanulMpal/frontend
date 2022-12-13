89d1880cf392abe60a0cc25d164cfaa3d23e04d1
hcanulMpal <116234411+hcanulMpal@users.noreply.github.com>
2022-12-13 17:32:53 -0500

    Cambios en user

--- src/modules/globals/axiosSL.js
+++ src/modules/globals/axiosSL.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 import { BACKEND } from './constans';
+import { getToken, isAuthenticated, logout } from '../login/helper/auth';
 
 export const apiLogin = axios.create({
     baseURL: BACKEND,
+})
+
+apiLogin.interceptors.request.use(function(req){
+    if(!isAuthenticated()){
+        logout();
+    }
+    else{
+        req.headers['Authorization'] = `Bearer ${getToken()}`
+    }
+    return req
 })
 
 apiLogin.interceptors.response.use(res => {
